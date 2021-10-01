2c1ac50822ec09aacd1b3f9f549c84c188c9a372
Zubier Hagi <zubier@Zubiers-MacBook-Pro.local>
2021-10-01 03:43:28 -0600

    Lab4

--- Labs/Lab_4/mysite/settings.py
+++ Labs/Lab_4/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#w_tek(w@4nqdeg(bj$(e$^8p^!ai-pj!86q#o$47l&#4huuzd'
+SECRET_KEY = os.getenv('Django_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
